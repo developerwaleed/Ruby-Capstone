40e5adef2c411864ec5e945601f52e29a865b51b
chrissiku <chrissiku5@gmail.com>
2022-09-05 17:42:29 +0200

    Implement methods for the `Item` class

--- classes/item.rb
+++ classes/item.rb
@@ -3,13 +3,21 @@ class Item
     attr_reader :id, :archived
     attr_accessor :genre, :author, :source, :label, :publish_date
 
-    def intialized
+    def intialized(genre, author, source, label, publish_date, archived)
         @id = Random.rand(0..1000)
         @genre = genre
         @author = author
         @source = source
         @label = label
         @publish_date = publish_date
-        @archived = archived    
+        @archived = archived
+    end
+
+    def can_be_removed?
+        (Date.today.year - @publish_date.year).to_i > 10
+    end
+
+    def move_to_archive
+        @archived = true if can_be_removed?
     end
 end
