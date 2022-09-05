a01c4969d5b9c84a30ea16fb857847f068c63491
chrissiku <chrissiku5@gmail.com>
2022-09-05 18:55:25 +0200

    Create class `Item`

---
+++ classes/label.rb
@@ -0,0 +1,13 @@
+require_relative   'item'
+
+class Label < Item
+    attr_reader :id, :items
+    attr_accessor :title, :color
+
+    def initialize(title, color)
+        @id = Random.rand(1..1000)
+        @title = title
+        @color = color
+        @items = []
+    end
+end
