90f0dbbf5cad50e8b2945a59e3eaf4d902071305
chrissiku <chrissiku5@gmail.com>
2022-09-05 21:48:53 +0200

    Add `add_label` method

--- app.rb
+++ app.rb
@@ -26,4 +26,17 @@ class App
             puts "#{book.cover_state} book published by #{book.publisher} on #{book.publish_date}"
         end
     end
+
+
+    def add_label
+        print "Label title : "
+        title = gets.chomp
+
+        print "Label color : "
+        color = gets.chomp
+
+        label = Label.new(title, color)
+        @labels.push(label)
+        puts "New Label added successfully"
+    end
 end 
