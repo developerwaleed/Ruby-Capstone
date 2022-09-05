72f211d93b657521c824d632f89d11772933ec51
chrissiku <chrissiku5@gmail.com>
2022-09-05 17:59:19 +0200

    Fix linters

--- classes/item.rb
+++ classes/item.rb
@@ -1,23 +1,22 @@
-
 class Item
-    attr_reader :id, :archived
-    attr_accessor :genre, :author, :source, :label, :publish_date
+  attr_reader :id, :archived
+  attr_accessor :genre, :author, :source, :label, :publish_date
 
-    def intialized(genre, author, source, label, publish_date, archived)
-        @id = Random.rand(0..1000)
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
-        @publish_date = publish_date
-        @archived = archived
-    end
+  def intialized(genre, author, source, label, publish_date, archived)
+    @id = Random.rand(0..1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
 
-    def can_be_removed?
-        (Date.today.year - @publish_date.year).to_i > 10
-    end
+  def can_be_removed?
+    (Date.today.year - @publish_date.year).to_i > 10
+  end
 
-    def move_to_archive
-        @archived = true if can_be_removed?
-    end
+  def move_to_archive
+    @archived = true if can_be_removed?
+  end
 end
