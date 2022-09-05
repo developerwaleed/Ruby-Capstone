d453e2655299d9a8363619e2306bec4a097a63d6
chrissiku <chrissiku5@gmail.com>
2022-09-05 21:02:34 +0200

    Test the `Label`'s properties

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -9,5 +9,17 @@ describe Label do
         it 'should be an instance of the Label class' do
             expect(@label).to be_an_instance_of Label
         end
+
+        it 'should have a title My label' do
+            expect(@label.title).to eq('My label')
+        end
+
+        it 'should have a color red' do
+            expect(@label.color).to eq('red')
+        end
+
+        it 'Chech the id is a number' do
+            expect(@label.id).to be_an_instance_of Integer
+        end
     end
 end
