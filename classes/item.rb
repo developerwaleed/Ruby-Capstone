e0528cbb7108d17461cdd5e2352a10d74c68bfd1
haroldmud <haroldmudosa@gmail.com>
2022-09-06 14:41:14 +0200

    Fix linters error

--- classes/item.rb
+++ classes/item.rb
@@ -1,21 +1,21 @@
 
-class Item 
-    attr_accessor :publish_date
-    attr_reader :genre, :label, :author, :archived, :source
-  
-    def initialize(date)
-      @id = Random.rand(1..1000)
-      @publish_date = Date.strptime(date, '%Y-%m-%d')
-      @archived = false
-    end
+class Item
+  attr_accessor :publish_date
+  attr_reader :genre, :label, :author, :archived, :source
 
-    def can_be_archived?
-      (Date.today.year - @publish_date.year) > 10
-    end
-  
-    def move_to_archive
-      true if can_be_archived?
-    end
-  
-    private :can_be_archived?
+  def initialize(date)
+    @id = Random.rand(1..1000)
+    @publish_date = Date.strptime(date, '%Y-%m-%d')
+    @archived = false
+  end
+
+  def can_be_archived?
+    (Date.today.year - @publish_date.year) > 10
+  end
+
+  def move_to_archive
+    true if can_be_archived?
+  end
+
+  private :can_be_archived?
 end
