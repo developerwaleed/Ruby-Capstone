768adfc9cc10e6e508bac83da451ffb55b11d5ba
Waleed Amjad <waleedamjad99@hotmail.com>
2022-09-06 13:29:43 +0500

    Add 'item methods'

--- classes/item.rb
+++ classes/item.rb
@@ -8,4 +8,14 @@ class Item
       @publish_date = Date.strptime(date, '%Y-%m-%d')
       @archived = false
     end
+
+    def can_be_archived?
+      (Date.today.year - @publish_date.year) > 10
+    end
+  
+    def move_to_archive
+      true if can_be_archived?
+    end
+  
+    private :can_be_archived?
 end
