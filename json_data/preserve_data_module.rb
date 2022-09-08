9426c7c3e57e9f23ad09894a6f0549c809c74123
Waleed Amjad <waleedamjad99@hotmail.com>
2022-09-08 15:15:27 +0500

    Fix minor bugs

--- json_data/preserve_data_module.rb
+++ json_data/preserve_data_module.rb
@@ -6,10 +6,15 @@ module PreserveJsonData
   end
 
   def load_json_data(json_file)
+    data = []
+    file = "./json_data/#{json_file}.json"
+    return data unless File.exist?(file) && File.read(file) != ''
+
     if File.empty?("./json_data/#{json_file}.json")
       puts 'No data in the catalog'
     else
-      JSON.parse(File.read("./json_data/#{json_file}.json"))
+      fetched_data = JSON.parse(File.read("./json_data/#{json_file}.json"))
+
     end
   end
 end
